96143081358359faca8b7c352443b0e34885a799
cepenicahs <cepenicah@gmail.com>
2022-02-25 21:35:15 +0800

    Button

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,13 +16,11 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+        bool value = false;
+        String operation = "";
+        Double result = 0;
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
         {
 
         }
@@ -52,12 +50,22 @@ namespace WindowsFormsApp1
 
         }
 
-        private void button34_Click(object sender, EventArgs e)
+        private void Numbers(object sender, EventArgs e)
         {
+            if ((txtDisplay.Text == "0") || (value)) txtDisplay.Text = "";
+            value = false;
 
+            Button num = (Button)sender;
+            if (num.Text == ".")
+            {
+                if (!txtDisplay.Text.Contains("."))
+                    txtDisplay.Text = txtDisplay.Text + num.Text;
+            }
+            else
+                txtDisplay.Text = txtDisplay.Text + num.Text;
         }
 
-        private void button24_Click(object sender, EventArgs e)
+        private void arithmetic(object sender, EventArgs e)
         {
 
         }
